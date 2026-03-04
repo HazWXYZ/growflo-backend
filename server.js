482671df79e86c7d55d20294699e4e0338129681
HazWXYZ <hazwxyz@gmail.com>
2026-03-04 10:40:58 -0600

    routes

--- server.js
+++ server.js
@@ -1,22 +1,10 @@
 import express from "express"
-
+import myplantsRoutes from "./routes/myplantsRoutes.js"
 const app = express ();
 
-app.get("/api/myplants", (req,res) =>{
-    res.status(200).send("Tracking plants");
-});
+app.use("/api/myplants", myplantsRoutes)
 
-app.post("/api/myplants", (req,res) =>{
-    res.status(201).send("Plant has been created successfully");
-});
 
-app.put("api/myplants/:id", (req,res) =>{
-    res.status(200).json({message: "Plant updated"})
-})
-
-app.delete("api/myplants/:id", (req,res) =>{
-    res.status(200).json({message: "Plant deleted"})
-})
 
 app.listen(5001, () => {
     console.log("Server started on PORT: 5001");
